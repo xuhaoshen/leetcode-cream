7a134bd21554466213faf1bc5975761e3361875c
shenxuhao <shenxuhao@9tongmail.com>
2020-05-29 15:32:43 +0800

    input new problem

--- src/main/java/algorithm/array/leetcode_75/Solution.java
+++ src/main/java/algorithm/array/leetcode_75/Solution.java
@@ -9,28 +9,11 @@ package algorithm.array.leetcode_75;
 public class Solution {
 
     public void sortColors(int[] nums) {
-        int left = 0;
-        int current = 0;
-        int right = nums.length - 1;
-        int tmp;
-        while (current <= right) {
-            if(nums[current] == 0) {
-                tmp = nums[left];
-                nums[left++] = nums[current];
-                nums[current++] = tmp;
 
-            } else if(nums[current] == 2) {
-                tmp = nums[right];
-                nums[right--] = nums[current];
-                nums[current] = tmp;
-            } else {
-                current++;
-            }
-        }
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.sortColors(new int[]{1,2,0});
+        solution.sortColors(new int[]{2, 0, 2, 1, 1, 0});
     }
 }
